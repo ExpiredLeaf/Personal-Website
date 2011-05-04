8824d6808d889dff02cd4714e984b926f11882f9
Manuel João Macedo <macmacedo93@gmail.com>
2011-05-05 00:50:26 +0100

    Tried something new on the twitter feed reader in version 1 but to no success, have no idea why

--- Version 1/scripts/tFeed.js
+++ Version 1/scripts/tFeed.js
@@ -26,8 +26,10 @@ jQuery(function(){
 	var script=document.createElement('script');
 	script.type='text/javascript';
 	//script.src= "http://twitter.com/statuses/user_timeline/26981005.json?&callback=processTheseTweets&_="+ new Date().getTime();
-	//script.src= "http://search.twitter.com/search.json?&q=apple&callback=processTheseTweets&_="+ new Date().getTime();
-	script.src="http://api.twitter.com/1/statuses/user_timeline.json?user_id=26981005&count=5&include_rts=1";
+	script.src= "http://search.twitter.com/search.json?&q=apple&callback=processTheseTweets&count=3&_="+ new Date().getTime();
+	//script.src="http://api.twitter.com/1/statuses/user_timeline.json?user_id=26981005&count=5&include_rts=1";
+	//script.src="http://twitter.com/statuses/user_timeline/aauminho.json?callback=processTheseTweets&count=1";
+	//script.src= "http://twitter.com/statuses/user_timeline/expiredleaf.json?callback=processTheseTweets&count=3";
 	processTheseTweets(script);
 
 	// Add the Script to the Body element, which will in turn load the script and run it.
